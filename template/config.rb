67f186bd9864cd864a64f362fbfeb388f0e9fe94
Oscar Palmér <opalmer@me.com>
2019-01-15 22:56:11 +0100

    comments
    + comments for various parts that are
      meant to be changed or extended

--- template/config.rb
+++ template/config.rb
@@ -1,31 +1,45 @@
+
+# External pipeline for Middleman
 activate :external_pipeline,
   name: :gulp,
   command: "./node_modules/gulp/bin/gulp.js #{build? ? 'build --production' : 'watch'}",
   source: '.tmp/gulp',
   latency: 1
 
+# Live reloading of assets (CSS, JS) during development
 activate :livereload
 
+# What to do when building
 configure :build do
+  # Suffixed hashes for assets; useful for caching
   activate :asset_hash
+  # Directory indexes for pages (prettier urls); e.g. about.html -> about/index.html
   activate :directory_indexes
+  # Compress all files
   activate :gzip
+  # Minify HTML
   activate :minify_html
 end
 
+# Ignore .keep-files; prevents empty folders in builds
 ignore '/**/.keep'
+# Ignore Sass-files; Gulp handles the pre-processing
 ignore '/**/*.{sass,scss}'
 
+# Ignore layouts for data-files
 page '/*.json',   layout: false
 page '/*.txt',    layout: false
 page '/*.xml',    layout: false
+# Ignore a directory index for the error page
 page '/404.html', directory_index: false
 
+# Where Middleman will look for assets
 set :css_dir,    'assets/stylesheets'
 set :fonts_dir,  'assets/fonts'
 set :images_dir, 'assets/images'
 set :js_dir,     'assets/javascripts'
 
+# Tell the Markdown-parser to allow more functionality
 set :markdown,
   autolink: true,
   fenced_code_blocks: true,
@@ -36,4 +50,5 @@ set :markdown,
   tables: true,
   with_toc_data: true
 
+# Set the Markdown engine to Redcarpet
 set :markdown_engine, :redcarpet
--- template/gulpfile.js
+++ template/gulpfile.js
@@ -1,3 +1,5 @@
+
+// Include all required modules
 const del = require('del');
 const gulp = require('gulp');
 const gulpif = require('gulp-if');
@@ -7,6 +9,7 @@ const uglify = require('gulp-uglify');
 const pump = require('pump');
 const yargs = require('yargs').argv;
 
+// Configuration for Gulp
 const config = {
   css: {
     destination: '.tmp/gulp/assets/stylesheets',
@@ -17,35 +20,45 @@ const config = {
     source: './source/assets/javascripts/**/*.js'
   },
   watch: {
+    // Allow tasks to be triggered on startup,
+    // not just after their first change
     ignoreInitial: false
   }
 };
 
+// Empty the temporary folder
 const clean = () => del(['.tmp/**']);
 
+// Gulp task for working with CSS and Sass
 const css = () => {
   return pump([
     gulp.src(config.css.source),
     sass().on('error', sass.logError),
+    // Minify if called during build
     gulpif(yargs.production === true, minify()),
     gulp.dest(config.css.destination)
   ]);
 };
 
+// Gulp task for working with JS
 const js = () => {
   return pump([
     gulp.src(config.js.source),
+    // Minify if called during build
     gulpif(yargs.production === true, uglify()),
     gulp.dest(config.js.destination)
   ]);
 };
 
+// Gulp task for watching files during development
 const watch = (cb) => {
   gulp.watch(config.css.source, config.watch, css);
   gulp.watch(config.js.source, config.watch, js);
 
+  // Gulp-callback for async goodness
   cb();
 };
 
+// Export the tasks for access in the CLI
 exports.build = gulp.series(clean, gulp.parallel(css, js), clean);
 exports.watch = gulp.series(clean, watch);
